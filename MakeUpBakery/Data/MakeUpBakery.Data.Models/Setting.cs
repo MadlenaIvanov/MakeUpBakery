632e9ab287a449511efc4863e508fe1e55ad908d
Madlena Ivanova <madlena.iv@abv.bg>
2022-01-12 14:41:04 +0200

    Initial commit - MakeUpBakery

---
+++ MakeUpBakery/Data/MakeUpBakery.Data.Models/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace MakeUpBakery.Data.Models
+{
+    using MakeUpBakery.Data.Common.Models;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ MakeUpBakery/MakeUpBakery.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace MakeUpBakery.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "MakeUpBakery";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
